4c92f53d5b3ce7183a30bf1a8412105b10cc4cc7
Henrique Assis <henriquebatista061@gmail.com>
2023-02-02 16:13:56 -0300

    projeto criado

---
+++ NanoChallenge007/NanoChallenge007/NanoChallenge007App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge007App.swift
+//  NanoChallenge007
+//
+//  Created by Henrique Assis on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge007App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
