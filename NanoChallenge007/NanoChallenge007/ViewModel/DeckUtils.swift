af91c818001b8e6e82cf69fbbb028868cf01fb28
Henrique Assis <henriquebatista061@gmail.com>
2023-02-03 15:48:18 -0300

    funcao para call de api e json

--- NanoChallenge007/NanoChallenge007/ViewModel/DeckUtils.swift
+++ NanoChallenge007/NanoChallenge007/ViewModel/DeckUtils.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 
-class DeckUtils {
-    private func apiCall() {
+class DeckUtils : ObservableObject {
+    /**
+     * Faz a chamada da API e a converte o JSON para o objeto, retornando o objeto deste tipo.
+     * - url: Passe a url da api nesse parâmetro.
+     * - object: Passe o modelo do tipo "Deck" que irá receber o JSON (no caso, ShuffleModel ou DrawModel)
+     */
+    private func apiCallJson<T:Deck>(url: URL, object: T.Type) -> (any Deck)? {
+        // api call
+        var deckReturn: (any Deck)? = nil
         
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            // convert to json
+            do {
+                let decodedDeck = try JSONDecoder().decode(object.self, from: data)
+                deckReturn = decodedDeck
+            }
+            catch {
+                print(error)
+            }
+        }
+        if deckReturn == nil { print("AVISO: o objeto solicitado para API está nulo, algo deu ruim. Confira o link certinho, e se está com o deck_id certinho também.") }
+        
+        return deckReturn ?? nil
     }
 }
