79e3e1385642b08964b0c162602cf00cf0f3e1ea
Barbara Argolo <92699920+BarbaraAM@users.noreply.github.com>
2023-02-03 15:48:34 -0300

    Reshuffledaorinha

--- NanoChallenge007/NanoChallenge007/ContentView.swift
+++ NanoChallenge007/NanoChallenge007/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var reshuffle : ShuffleModel? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Ola")
+            .padding()
+            .onAppear() {
+                DeckUtils().getReshuffle { (reshuffle) in
+                    self.reshuffle = reshuffle
+                }
+            }.navigationTitle("Olar")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- NanoChallenge007/NanoChallenge007/ViewModel/DeckUtils.swift
+++ NanoChallenge007/NanoChallenge007/ViewModel/DeckUtils.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
-class DeckUtils {
+class DeckUtils : ObservableObject {
+    @Published var reshuffle : ShuffleModel? = nil
     
+    func getReshuffle(completion: @escaping (ShuffleModel) -> ()) {
+        guard let url = URL(string: "https://deckofcardsapi.com/api/deck/lvqblg4m6zc8/shuffle/") else {
+            print("nao deu derto")
+            return
+        }
+        URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            let reshuffle = try! JSONDecoder().decode(ShuffleModel.self, from: data!)
+            print(reshuffle)
+            DispatchQueue.main.async {
+                completion(reshuffle)
+            }
+        }.resume()
+    }
 }
