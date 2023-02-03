51441fc0d9aff8c43cf04fbf22372116a8dfb8af
Henrique Assis <henriquebatista061@gmail.com>
2023-02-03 15:06:52 -0300

    view populada

--- NanoChallenge007/NanoChallenge007/View/HomeView.swift
+++ NanoChallenge007/NanoChallenge007/View/HomeView.swift
@@ -1,14 +1,8 @@
 //
-//  DeckUtils.swift
+//  HomeView.swift
 //  NanoChallenge007
 //
 //  Created by Henrique Assis on 03/02/23.
 //
 
 import Foundation
-
-class DeckUtils {
-    private func apiCall() {
-        
-    }
-}
